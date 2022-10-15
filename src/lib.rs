ea0cf35a0d8ae3e10edaefb5643e180cd4172bde
josepmdc <jdomingocatafal@gmail.com>
2022-10-15 13:06:28 +0200

    Cleanup lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
-use std::{
-    ffi::{CStr, c_void},
-    io::{stderr, Write},
-};
+use std::ffi::{c_void, CStr};
 
 use llvm_sys::support::LLVMAddSymbol;
 
@@ -11,59 +8,14 @@ macro_rules! cstr {
     };
 }
 
-static PUTCHARD_NAME: &'static CStr = cstr!("putchard");
-#[no_mangle]
-pub extern "C" fn putchard(x: f64) -> f64 {
-    let mut stderr = stderr();
-    match write!(stderr, "{}", x as u8 as char).ok() {
-        Some(()) => (),
-        None => return 1.0,
-    }
-    match stderr.flush().ok() {
-        Some(()) => (),
-        None => return 1.0,
-    }
-
-    0.0
-}
-
 static PRINT_NAME: &'static CStr = cstr!("print");
 #[no_mangle]
-pub extern "C" fn print(x: f64) -> f64 {
-    let mut stderr = stderr();
-    match writeln!(stderr, "{}", x).ok() {
-        Some(()) => (),
-        None => return 1.0,
-    }
-    match stderr.flush().ok() {
-        Some(()) => (),
-        None => return 1.0,
-    }
-
-    0.0
+pub extern "C" fn print(x: f64) {
+    println!("{}", x);
 }
-
-pub struct PrintFunc {
-    pub name: &'static CStr,
-    pub func_pointer: extern "C" fn(f64) -> f64,
-}
-
-#[used]
-pub static PRINT_FNS: [PrintFunc; 2] = [
-    PrintFunc {
-        name: PUTCHARD_NAME,
-        func_pointer: putchard,
-    },
-    PrintFunc {
-        name: PRINT_NAME,
-        func_pointer: print,
-    },
-];
 
 pub fn shim_lib_functions() {
-    for func in &PRINT_FNS {
-        let fn_name = func.name.as_ptr();
-        let fn_ptr = func.func_pointer as *mut c_void;
-        unsafe { LLVMAddSymbol(fn_name, fn_ptr) };
-    }
+    let fn_name = PRINT_NAME.as_ptr();
+    let fn_ptr = print as *mut c_void;
+    unsafe { LLVMAddSymbol(fn_name, fn_ptr) };
 }
